b6460116c18d710b782ca3cab28c5e814d476542
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-02-28 22:42:26 -0500

    output of npm command at the bottom

---
+++ second.js
@@ -0,0 +1,54 @@
+/*
+connected an output stream to the bottom box input
+*/
+const spawn = require('cross-spawn-async')
+const blessed = require('blessed')
+
+const screen = blessed.screen({
+  smartCSR: true,
+  dockBorders: true,
+  ignoreDockContrast: true
+})
+screen.title = 'my window title'
+
+const topOutput = blessed.box({
+  label: 'top',
+  left: 0,
+  top: 0,
+  border: 'line',
+  content: 'top output',
+  style: {
+    width: '100%',
+    height: '50%'
+  }
+})
+
+const npmOutput = blessed.log({
+  content: 'bottom output',
+  label: 'bottom',
+  left: 0,
+  top: '50%-1',
+  border: 'line',
+  keys: true,
+  style: {
+    scrollbar: true,
+    width: '100%',
+    height: '50%'
+  }
+})
+
+screen.append(topOutput)
+screen.append(npmOutput)
+
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  console.log('escaped')
+  process.exit(0)
+})
+
+npmOutput.focus()
+screen.render()
+
+const proc = spawn('npm', ['help'])
+proc.stdout.on('data', (data) => {
+  npmOutput.add(data.toString())
+})
